ce3eab4b36b02ac486d7e4bd14969fe3a1bd783b
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-09-01 19:23:19 +0300

    Initial Commit

---
+++ Habitly/HabitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitlyApp.swift
+//  Habitly
+//
+//  Created by newbie on 01.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
